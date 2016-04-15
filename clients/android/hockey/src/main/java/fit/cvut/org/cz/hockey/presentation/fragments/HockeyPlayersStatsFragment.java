ab2530941610f3527b123067913e45db3ae3a943
kosutond <kosutond@fit.cvut.cz>
2016-04-15 19:43:14 +0200

    Add players to hockey tour and comp. Refresh i then too fast, needs to be fixed

--- clients/android/hockey/src/main/java/fit/cvut/org/cz/hockey/presentation/fragments/HockeyPlayersStatsFragment.java
+++ clients/android/hockey/src/main/java/fit/cvut/org/cz/hockey/presentation/fragments/HockeyPlayersStatsFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
@@ -13,8 +14,11 @@ import android.view.ViewGroup;
 
 import fit.cvut.org.cz.hockey.R;
 import fit.cvut.org.cz.hockey.business.entities.AgregatedStatistics;
+import fit.cvut.org.cz.hockey.presentation.activities.AddPlayersActivity;
 import fit.cvut.org.cz.hockey.presentation.adapters.AgregatedStatisticsAdapter;
+import fit.cvut.org.cz.hockey.presentation.services.PlayerService;
 import fit.cvut.org.cz.hockey.presentation.services.StatsService;
+import fit.cvut.org.cz.tmlibrary.presentation.activities.SelectableListActivity;
 import fit.cvut.org.cz.tmlibrary.presentation.adapters.AbstractListAdapter;
 import fit.cvut.org.cz.tmlibrary.presentation.fragments.AbstractListFragment;
 
@@ -28,6 +32,8 @@ public class HockeyPlayersStatsFragment extends AbstractListFragment<AgregatedSt
     private static String ARG_COMP_ID = "competition_id";
     private static String ARG_TOUR_ID = "tournament_id";
 
+    private BroadcastReceiver statsReceiver = new StatsReceiver();
+
     public static HockeyPlayersStatsFragment newInstance( long id, boolean forComp )
     {
         HockeyPlayersStatsFragment fragment = new HockeyPlayersStatsFragment();
@@ -36,21 +42,21 @@ public class HockeyPlayersStatsFragment extends AbstractListFragment<AgregatedSt
         if( forComp ) args.putLong(ARG_COMP_ID, id);
         else args.putLong(ARG_TOUR_ID, id);
 
-        fragment.setArguments( args );
+        fragment.setArguments(args);
         return fragment;
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         if( getArguments() != null ) {
             competitionID = getArguments().getLong(ARG_COMP_ID, -1);
             tournamentID = getArguments().getLong(ARG_TOUR_ID, -1);
         }
-
-        return super.onCreateView(inflater, container, savedInstanceState);
-        
     }
+
 
     @Override
     protected AbstractListAdapter getAdapter() {
@@ -94,17 +100,19 @@ public class HockeyPlayersStatsFragment extends AbstractListFragment<AgregatedSt
 
         if( competitionID != -1 ) {
             filter = new IntentFilter(StatsService.ACTION_GET_BY_COMP_ID);
+            filter.addAction( PlayerService.ACTION_ADD_PLAYERS_TO_COMPETITION );
         }
         else {
             filter = new IntentFilter(StatsService.ACTION_GET_BY_TOUR_ID);
+            filter.addAction( PlayerService.ACTION_ADD_PLAYERS_TO_TOURNAMENT );
         }
 
-        LocalBroadcastManager.getInstance(getContext()).registerReceiver( receiver, filter);
+        LocalBroadcastManager.getInstance(getContext()).registerReceiver( statsReceiver, filter);
     }
 
     @Override
     protected void unregisterReceivers() {
-        LocalBroadcastManager.getInstance( getContext() ).unregisterReceiver(receiver);
+        LocalBroadcastManager.getInstance( getContext() ).unregisterReceiver(statsReceiver);
     }
 
     @Override
@@ -115,6 +123,22 @@ public class HockeyPlayersStatsFragment extends AbstractListFragment<AgregatedSt
                                    @Override
                                    public void onClick(View v) {
 
+                                       int requestCode;
+                                       int option;
+                                       long id;
+                                       if (competitionID != -1) {
+                                           option = AddPlayersFragment.OPTION_COMPETITION;
+                                           id = competitionID;
+                                           requestCode = option;
+                                       } else {
+                                           option = AddPlayersFragment.OPTION_TOURNAMENT;
+                                           id = tournamentID;
+                                           requestCode = option;
+                                       }
+
+                                       Intent intent = AddPlayersActivity.newStartIntent(getContext(), option, id);
+
+                                       startActivityForResult(intent, requestCode);
                                    }
                                }
         );
@@ -122,20 +146,59 @@ public class HockeyPlayersStatsFragment extends AbstractListFragment<AgregatedSt
         return fab;
     }
 
-//    public class StatsReceiver extends BroadcastReceiver
-//    {
-//
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//            String action = intent.getAction();
-//
-//            switch (action)
-//            {
-//                case StatsService.ACTION_GET_BY_TOUR_ID:
-//                case StatsService.ACTION_GET_BY_COMP_ID:
-//
-//            }
-//        }
-//    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode != SelectableListActivity.RESULT_OK) return;
+
+        if( competitionID != -1 ) {
+            Intent intent = PlayerService.newStartIntent(PlayerService.ACTION_ADD_PLAYERS_TO_COMPETITION, getContext());
+            intent.putParcelableArrayListExtra(PlayerService.EXTRA_PLAYERS, data.getParcelableArrayListExtra(AddPlayersActivity.EXTRA_DATA));
+            intent.putExtra(PlayerService.EXTRA_ID, competitionID);
+            getContext().startService(intent);
+        }
+        else {
+            Intent intent = PlayerService.newStartIntent(PlayerService.ACTION_ADD_PLAYERS_TO_TOURNAMENT, getContext());
+            intent.putParcelableArrayListExtra(PlayerService.EXTRA_PLAYERS, data.getParcelableArrayListExtra(AddPlayersActivity.EXTRA_DATA));
+            intent.putExtra(PlayerService.EXTRA_ID, tournamentID);
+            getContext().startService(intent);
+        }
+        if( progressInterface != null )
+            progressInterface.showProgress();
+    }
+
+    public class StatsReceiver extends BroadcastReceiver
+    {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            String getAction;
+            String addAction;
+
+            if( competitionID != -1 ) {
+                getAction = StatsService.ACTION_GET_BY_COMP_ID;
+                addAction = PlayerService.ACTION_ADD_PLAYERS_TO_COMPETITION;
+            }
+            else {
+                getAction = StatsService.ACTION_GET_BY_TOUR_ID;
+                addAction = PlayerService.ACTION_ADD_PLAYERS_TO_TOURNAMENT;
+            }
+
+            switch (action)
+            {
+                case StatsService.ACTION_GET_BY_TOUR_ID:
+                case StatsService.ACTION_GET_BY_COMP_ID:
+                    HockeyPlayersStatsFragment.super.bindDataOnView(intent);
+                    if (!PlayerService.isWorking(addAction) && !StatsService.isWorking(getAction) && progressInterface != null)
+                        progressInterface.hideProgress();
+                    break;
+                case PlayerService.ACTION_ADD_PLAYERS_TO_TOURNAMENT:
+                case PlayerService.ACTION_ADD_PLAYERS_TO_COMPETITION:
+                    break;
+
+            }
+        }
+    }
 
 }
