6a44446f3f267a95d055db49be1c724342179e0f
kosutond <kosutond@fit.cvut.cz>
2016-05-02 19:41:04 +0200

    Updated Hockey Service

--- clients/android/hockey/src/main/java/fit/cvut/org/cz/hockey/presentation/services/HockeyService.java
+++ clients/android/hockey/src/main/java/fit/cvut/org/cz/hockey/presentation/services/HockeyService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import java.util.ArrayList;
 
+import fit.cvut.org.cz.hockey.R;
 import fit.cvut.org.cz.hockey.business.ManagerFactory;
 import fit.cvut.org.cz.hockey.business.entities.AggregatedStatistics;
 import fit.cvut.org.cz.tmlibrary.business.AggregatedStats;
@@ -38,15 +39,26 @@ public class HockeyService extends AbstractIntentServiceWProgress {
             {
                 Intent res = new Intent(action);
                 ArrayList<AggregatedStatistics> stats = ManagerFactory.getInstance().statisticsManager.getAllAgregated( this );
-
-                //TODO prevest na jadrovou formu
-
-                // Ukazka posilani statistik
-                AggregatedStats as = new AggregatedStats();
-                as.addRecord(new AggregatedStatsRecord("G", "10"));
-                as.addRecord(new AggregatedStatsRecord("A", "5"));
-                as.addRecord(new AggregatedStatsRecord("Points", "8"));
-                res.putExtra(CrossPackageComunicationConstants.EXTRA_STATS, as);
+                ArrayList<AggregatedStats> statsToSend = new ArrayList<>();
+                for( AggregatedStatistics ags : stats ){
+                    AggregatedStats as = new AggregatedStats();
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.name), ags.getPlayerName()));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.g), Long.toString(ags.getGoals())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.a), Long.toString(ags.getAssists())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.p), Long.toString(ags.getPoints())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.pmp), Long.toString(ags.getPlusMinusPoints())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.tp), Long.toString(ags.getTeamPoints())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.gp), Long.toString(ags.getMatches())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.w), Long.toString(ags.getWins())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.d), Long.toString(ags.getDraws())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.l), Long.toString(ags.getLosses())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.ag), String.format("%.2f", ags.getAvgGoals())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.ap), String.format("%.2f", ags.getAvgPoints())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.apmp), String.format("%.2f", ags.getAvgPlusMinus())));
+                    as.addRecord(new AggregatedStatsRecord(getString(R.string.atp), String.format("%.2f", ags.getAvgTeamPoints())));
+                    statsToSend.add( as );
+                }
+                res.putParcelableArrayListExtra(CrossPackageComunicationConstants.EXTRA_STATS, statsToSend);
 
                 sendBroadcast( res );
                 break;
