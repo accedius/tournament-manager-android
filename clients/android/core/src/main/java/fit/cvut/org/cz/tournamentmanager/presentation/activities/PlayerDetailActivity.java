ad7369d0a7cb7aa70ac02901341a24e36db1c536
Josef Němeček <nemecjo2@fit.cvut.cz>
2016-10-07 18:36:48 +0200

    Codestyle
     - build imports
     - removed unused import

--- clients/android/core/src/main/java/fit/cvut/org/cz/tournamentmanager/presentation/activities/PlayerDetailActivity.java
+++ clients/android/core/src/main/java/fit/cvut/org/cz/tournamentmanager/presentation/activities/PlayerDetailActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.PagerAdapter;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
