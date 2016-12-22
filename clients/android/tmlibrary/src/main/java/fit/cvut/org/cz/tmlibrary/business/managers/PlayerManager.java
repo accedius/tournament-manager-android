43c1d22af599359a1375b0991e61a0ad8f7016a8
Josef Němeček <nemecjo2@fit.cvut.cz>
2016-12-22 16:09:25 +0100

    Default players order fixed.

--- clients/android/tmlibrary/src/main/java/fit/cvut/org/cz/tmlibrary/business/managers/PlayerManager.java
+++ clients/android/tmlibrary/src/main/java/fit/cvut/org/cz/tmlibrary/business/managers/PlayerManager.java
@@ -1,5 +1,9 @@
 package fit.cvut.org.cz.tmlibrary.business.managers;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import fit.cvut.org.cz.tmlibrary.business.managers.interfaces.IPlayerManager;
 import fit.cvut.org.cz.tmlibrary.data.entities.Player;
 
@@ -10,5 +14,23 @@ public class PlayerManager extends TManager<Player> implements IPlayerManager {
     @Override
     protected Class<Player> getMyClass() {
         return Player.class;
+    }
+
+    @Override
+    public List<Player> getAll() {
+        List<Player> players = super.getAll();
+        orderPlayers(players);
+        return players;
+    }
+
+    private void orderPlayers(List<Player> players) {
+        Collections.sort(players, new Comparator<Player>() {
+            @Override
+            public int compare(Player lp, Player rp) {
+                if (!rp.getName().equals(lp.getName()))
+                    return lp.getName().compareTo(rp.getName());
+                return lp.getEmail().compareTo(rp.getEmail());
+            }
+        });
     }
 }
